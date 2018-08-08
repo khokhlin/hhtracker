afc737ebf949fee90c6f011416483d97b319c673
Andrey Khokhlin <khokhlin@gmail.com>
2018-08-08 21:53:25 +0300

    Add argparse

--- hhtracker/hhtracker.py
+++ hhtracker/hhtracker.py
@@ -1,6 +1,6 @@
 """hhtracker - track new vacancies on hh.ru"""
-import sys
 import logging
+from argparse import ArgumentParser
 from datetime import datetime
 from datetime import timedelta
 import requests
@@ -9,9 +9,14 @@ import requests
 URL = "https://api.hh.ru/vacancies"
 MAX_TIMEOUT = 3
 MAX_PER_PAGE = 100
+MOSCOW_CODE = 1
+FMT = """{vacancy_name:<50.50} | {salary_from}/{salary_to} \
+({currency}) | {created_at}")
+{vacancy_url}
+{employer_name} ({employer_url})
+"""
 
-
-def show(vacancy):
+def show(vacancies):
     """
     Available keys:
     [
@@ -21,34 +26,33 @@ def show(vacancy):
         'id', 'name', 'address', 'premium'
     ]
     """
-    vacancy_id = vacancy.get("id")
-    vacancy_name = vacancy.get("name")
-    salary = vacancy.get("salary") or {}
-    currency = salary.get("currency") or "..."
-    salary_from = salary.get("from") or "..."
-    salary_to = salary.get("to") or "..."
-    gross = salary.get("gross")
-    employer = vacancy.get("employer")
-    employer_name = employer.get("name", "")
-    created_at = vacancy.get("created_at")
-
-    print(
-        "{vacancy_id}: {vacancy_name} | SALARY: {salary_from}/{salary_to}({currency}) "
-        "| CREATED: {created_at} | EMPLOYER: {employer_name}".format_map(locals())
-    )
+    for vacancy in vacancies:
+        vacancy_id = vacancy.get("id")
+        vacancy_name = vacancy.get("name")
+        vacancy_url = vacancy.get("url")
+        salary = vacancy.get("salary") or {}
+        currency = salary.get("currency") or "..."
+        salary_from = salary.get("from") or "..."
+        salary_to = salary.get("to") or "..."
+        gross = salary.get("gross")
+        employer = vacancy.get("employer")
+        employer_name = employer.get("name")
+        employer_url = employer.get("url")
+        created_at = vacancy.get("created_at")
+        print(FMT.format_map(locals()))
 
 
 def fetch_pages(params):
     headers = {'user-agent': 'hhtracker'}
-    params.update({
-        "per_page": MAX_PER_PAGE,
-        "page": 0
-    })
+    params["page"] = 0
     while True:
         try:
-            result = requests.get(URL, params=params,
-                                  headers=headers, timeout=MAX_TIMEOUT)
+            result = requests.get(URL,
+                                  params=params,
+                                  headers=headers,
+                                  timeout=MAX_TIMEOUT)
             result.raise_for_status()
+
         except (requests.exceptions.Timeout,
                 requests.exceptions.ConnectionError):
             logging.error("Unable to connect to server")
@@ -64,30 +68,33 @@ def fetch_pages(params):
             next_page = current_page + 1
             if next_page >= total_pages:
                 break
-            if params["page"] == next_page:
-                logging.error("Wrong page parameter")
-                break
             params["page"] = next_page
 
 
-def get_vacancies(text):
+def get_vacancies(text, region):
     date_from = datetime.now() - timedelta(hours=24)
     params = {
+        "per_page": MAX_PER_PAGE,
+        "area": region,
         "search_field": "name",
         "date_from": date_from.strftime("%Y%m%d"),
         "text": text
     }
     for page in fetch_pages(params):
-        for vacancy in page["items"]:
-            show(vacancy)
+        show(page["items"])
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--keywords", nargs="+", help="Search keywords")
+    parser.add_argument("--region", type=int, default=MOSCOW_CODE,
+                        help="Region code. (https://api.hh.ru/areas)")
+    return parser.parse_args()
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("search argument is required")
-        sys.exit(-1)
-
-    get_vacancies(text=sys.argv[1])
+    args = parse_args()
+    get_vacancies(text=" ".join(args.keywords), region=args.region)
 
 
 if __name__ == "__main__":
