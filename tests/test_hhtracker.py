be0b209c78533fc999aec202e96b6082bd45cc2d
Andrey Khokhlin <khokhlin@gmail.com>
2018-10-11 00:57:24 +0300

    Rewrite tests

--- tests/test_hhtracker.py
+++ tests/test_hhtracker.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+import pytest
 import requests_mock
 from hhtracker import config
 config.test = True
@@ -6,67 +6,70 @@ from hhtracker.hhtracker import get_vacancies
 from hhtracker.models import create_tables, drop_tables
 
 
-class TestHHTracker(TestCase):
+def setup_module(module):
+    create_tables()
 
-    @classmethod
-    def setUpClass(cls):
-        create_tables()
 
-    @classmethod
-    def tearDownClass(cls):
-        drop_tables()
+def teardown_module(module):
+    drop_tables()
 
-    @requests_mock.Mocker()
-    def test_vacancies_created(self, mocker):
-        resp = {"page": "0", "pages": 1, "items": []}
-        mocker.get(requests_mock.ANY, json=resp)
-        result = get_vacancies("python", 1)
-        self.assertEqual(result, [])
 
-        resp = {
-            "page": "0",
-            "pages": 1,
-            "items": [{
-                "id": 1,
-                "name": "Python developer",
-                "salary": "100000",
-                "currency": "RUB",
-                "employer": {
-                    "id": 1,
-                    "name": "Google"
-                }
-            }]
-        }
-        mocker.get(requests_mock.ANY, json=resp)
+@pytest.fixture(scope="module")
+def mocker():
+    with requests_mock.Mocker() as mocker:
+        yield mocker
 
-        result = get_vacancies("python", 1)
-        result = get_vacancies("python", 1)
 
-        result[0].pop("created_at")
+def test_vacancies_created(mocker):
+    resp = {"page": "0", "pages": 1, "items": []}
+    mocker.get(requests_mock.ANY, json=resp)
+    result = get_vacancies("python", 1)
+    assert result == []
 
-        expected = [{
-            'vacancy_id': 1,
-            'currency': 'RUB',
-            'employer': {'employer_id': 1, 'name': 'Google'},
-            'name': 'Python developer',
-            'salary': 100000
-        }]
-        self.assertEqual(result, expected)
-
-        resp["items"].append({
-            "id": 2,
-            "name": "Java Developer",
-            "salary": "2000000",
+    resp = {
+        "page": "0",
+        "pages": 1,
+        "items": [{
+            "id": 1,
+            "name": "Python developer",
+            "salary": "100000",
             "currency": "RUB",
             "employer": {
                 "id": 1,
-                "name": "Google",
+                "name": "Google"
             }
-        })
+        }]
+    }
+    mocker.get(requests_mock.ANY, json=resp)
 
-        items = get_vacancies("python", 1)
-        self.assertEqual(len(items), 2)
-        self.assertEqual(int(items[0]["vacancy_id"]), 1)
-        self.assertEqual(int(items[1]["vacancy_id"]), 2)
-        self.assertEqual(int(items[0]["employer"]["employer_id"]), 1)
-        self.assertEqual(int(items[1]["employer"]["employer_id"]), 1)
+    result = get_vacancies("python", 1)
+    result = get_vacancies("python", 1)
+
+    result[0].pop("created_at")
+
+    expected = [{
+        'vacancy_id': 1,
+        'currency': 'RUB',
+        'employer': {'employer_id': 1, 'name': 'Google'},
+        'name': 'Python developer',
+        'salary': 100000
+    }]
+    assert result == expected
+
+    resp["items"].append({
+        "id": 2,
+        "name": "Java Developer",
+        "salary": "2000000",
+        "currency": "RUB",
+        "employer": {
+            "id": 1,
+            "name": "Google",
+        }
+    })
+
+    items = get_vacancies("python", 1)
+    assert len(items) == 2
+    assert int(items[0]["vacancy_id"]) == 1
+    assert int(items[1]["vacancy_id"]) == 2
+    assert int(items[0]["employer"]["employer_id"]) == 1
+    assert int(items[1]["employer"]["employer_id"]) == 1
--- tests/test_ws.py
+++ tests/test_ws.py
@@ -1,5 +1,4 @@
-from unittest import TestCase
-
+import pytest
 from hhtracker import config
 config.test = True
 from hhtracker.models import create_tables, drop_tables
@@ -7,45 +6,47 @@ from hhtracker.models import Vacancy
 from hhtracker.ws import app
 
 
-class TestWebServer(TestCase):
+def setup_module(module):
+    create_tables()
 
-    @classmethod
-    def setUpClass(cls):
-        cls.client = app.test_client()
-        create_tables()
 
-    @classmethod
-    def tearDownClass(cls):
-        drop_tables()
+def teardown_module(module):
+    drop_tables()
 
-    def test_can_get_vacancies(self):
-        vacancy_params = {
-            "vacancy_id": 1,
-            "name": "Python Developer",
-            "employer": {
-                "employer_id": 1,
-                "name": "Google",
-            },
-            "currency": "RUB",
-            "salary": 100000,
-        }
+
+@pytest.fixture(scope="module")
+def client():
+    return app.test_client()
+
+
+def test_can_get_vacancies(client):
+    vacancy_params = {
+        "vacancy_id": 1,
+        "name": "Python Developer",
+        "employer": {
+            "employer_id": 1,
+            "name": "Google",
+        },
+        "currency": "RUB",
+        "salary": 100000,
+    }
+    Vacancy.create(**vacancy_params)
+    vacancy = client.get("/api/vacancies").get_json()[0]
+    assert vacancy_params["vacancy_id"] == vacancy["vacancy_id"]
+    assert vacancy_params["salary"] == vacancy["salary"]
+    assert vacancy_params["employer"] == vacancy["employer"]
+
+    for i in range(2, 10):
+        vacancy_params["vacancy_id"] = i
         Vacancy.create(**vacancy_params)
-        vacancy = self.client.get("/api/vacancies").get_json()[0]
-        self.assertEqual(vacancy_params["vacancy_id"], vacancy["vacancy_id"])
-        self.assertEqual(vacancy_params["salary"], vacancy["salary"])
-        self.assertEqual(vacancy_params["employer"], vacancy["employer"])
 
-        for i in range(2, 10):
-            vacancy_params["vacancy_id"] = i
-            Vacancy.create(**vacancy_params)
+    page1 = client.get("/api/vacancies").get_json()
+    assert len(page1) == 3
+    page2 = client.get("/api/vacancies").get_json()
 
-        page1 = self.client.get("/api/vacancies").get_json()
-        self.assertEqual(len(page1), 3)
-        page2 = self.client.get("/api/vacancies").get_json()
+    # Change the first record visibility
 
-        # Change the first record visibility
-
-        self.client.post("/api/vacancy/1", json={"visible": False})
-        resp = self.client.get("/api/vacancies?per_page=10").get_json()
-        self.assertEqual(len(resp), 8)
-        self.assertEqual(len([item for item in resp if item["vacancy_id"] == 1]), 0)
+    client.post("/api/vacancy/1", json={"visible": False})
+    resp = client.get("/api/vacancies?per_page=10").get_json()
+    assert len(resp) == 8
+    assert len([item for item in resp if item["vacancy_id"] == 1]) == 0
